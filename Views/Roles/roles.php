abb75538481090e0fad469e9005f401f790c10b1
CristianBarreto30 <cristianbarreto0205@gmail.com>
2025-08-20 10:27:21 -0500

    actualizacion en roles
    Agregue un comentario de que ludwin le gusta duvan

--- Views/Roles/roles.php
+++ Views/Roles/roles.php
@@ -26,6 +26,7 @@
                     <table class="table table-hover table-bordered" id="tableRoles">
                       <thead>
                         <tr>
+                          <!-- ludwin es re gei chupa-->
                           <th>ID</th>
                           <th>Nombre</th>
                           <th>Descripción</th>
