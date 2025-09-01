d0bad1a2822530e707b2db78bc174ce4239b82be
ludwinmayorga089 <ludwin.mayorga089@gmail.com>
2025-09-01 09:25:30 -0500

    actualizando config
    actualizando config para el despliegue

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootsstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <?php 
 	require_once("Config/Config.php");
 	require_once("Helpers/Helpers.php");
