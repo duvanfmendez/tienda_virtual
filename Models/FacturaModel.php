afa1126425280a458348895185856cf0642a076f
santival300 <tiagovalbuenacabadia@gmail.com>
2025-09-08 11:40:12 -0500

    Arreglo carrito paypal

--- Models/FacturaModel.php
+++ Models/FacturaModel.php
@@ -36,8 +36,6 @@
 										telefono,
 										email_user,
 										nit,
-										nombrefiscal,
-										direccionfiscal 
 								FROM persona WHERE idpersona = $idpersona ";
 				$requestcliente = $this->select($sql_cliente);
 				$sql_detalle = "SELECT p.idproducto,
