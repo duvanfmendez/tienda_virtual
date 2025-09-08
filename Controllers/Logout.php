89e8b2b752affe602b9e7e140f268185e2cef394
CristianBarreto30 <208307848+CristianBarreto30@users.noreply.github.com>
2025-09-08 08:00:19 -0500

    Comentarios en controllers

--- Controllers/Logout.php
+++ Controllers/Logout.php
@@ -1,11 +1,12 @@
 <?php
 	class Logout
 	{
+		// Constructor que finaliza la sesión del usuario y lo redirige al login
 		public function __construct()
 		{
-			session_start();
-			session_unset();
-			session_destroy();
+			session_start();  // Inicia la sesión para manipularla
+			session_unset();  // Limpia todas las variables de sesión
+			session_destroy();		// Destruye la sesión actual
 			header('location: '.base_url().'/login');
 			die();
 		}
