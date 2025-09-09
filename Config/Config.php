ce4b6c6bd902fda301d099020d997dca20cd054b
duvanfmendez <duvanmendez1029@gmail.com>
2025-09-09 14:07:27 -0500

    arreglos

--- Config/Config.php
+++ Config/Config.php
@@ -4,23 +4,6 @@
 
 	const BASE_URL = "http://localhost/tienda_virtual/";
 
-	//Zona horaria
-
-
-	//Datos de conexión a Base de Datos
-	const DB_HOST = "localhost";
-	const DB_NAME = "db_tiendavirtual";
-	const DB_USER = "root";
-	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";/*/
-
-	//Variables de hosting
-	
-	const BASE_URL = "http://localhost/tienda_virtual";
-
-	//Zona horaria
-	date_default_timezone_set('America/Guatemala');
-
 	//Datos de conexión a Base de Datos
 	const DB_HOST = "localhost";
 	const DB_NAME = "db_tiendavirtual";
