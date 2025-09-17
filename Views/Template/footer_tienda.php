31c9d6ce5f27692586ac542d86e9c5b29a3ea43b
CristianBarreto30 <208307848+CristianBarreto30@users.noreply.github.com>
2025-09-17 09:36:11 -0500

    Chat bot

--- Views/Template/footer_tienda.php
+++ Views/Template/footer_tienda.php
@@ -2,6 +2,22 @@
 		$catFotter = getCatFooter();
 	 ?>
 	<!-- Footer -->
+	 <script type="text/javascript">
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '68c2c510d86efd20b850ccef' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
 	<footer class="bg3 p-t-75 p-b-32">
 		<div class="container">
 			<div class="row">
